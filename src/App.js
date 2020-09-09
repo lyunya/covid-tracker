e228eadffea3a7f486b8cec729aebb9075dd1315
Leon Marbukh <leonmarbukh@gmail.com>
2020-09-09 16:16:51 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { Cards, Chart, CountryPicker } from './components';
+import { fetchData } from './api';
+import styles from './App.module.css';
+
+class App extends React.Component {
+
+   async componentDidMount() {
+        const data = await fetchData();
+
+        console.log(data);
+    }
+    render() {
+        return  (
+            <div className={styles.container}>
+                <Cards />
+                <CountryPicker />
+                <Chart />
+            </div>
+        )
+    }
+}
+
+export default App;
